db79ca7e6ba14ce80647071c079d4ac3e989e228
Mr.tao <staugur@vip.qq.com>
2017-05-28 17:51:33 +0800

    百度收录插件

--- src/plugins/BaiduIncludedCheck/util.py
+++ src/plugins/BaiduIncludedCheck/util.py
@@ -37,7 +37,7 @@ class BaiduIncludedCheckUtil(PluginBase):
         else:
             # 设置UA模拟用户，还可设置多个UA提高搜索成功率
             headers = {'User-Agent': 'Mozilla/4.0+(compatible;+MSIE+8.0;+Windows+NT+5.1;+Trident/4.0;+GTB7.1;+.NET+CLR+2.0.50727)'}
-            # 构建百度搜索URL；因为是查收录，所以只显示了前10个搜索结果，还可以通过rn参数来调整搜索结果的数量
+            # 构建百度搜索URL；因为是查收录，所以只显示了前1个搜索结果，还可以通过rn参数来调整搜索结果的数量，不过结果数量越多，速度越慢，因为每个结果都要请求下获取真实URL。
             b_url = 'http://www.baidu.com/s?wd=%s&rn=1' % url
             # 初始化BeautifulSoup
             soup = BeautifulSoup(requests.get(b_url, headers=headers, timeout=self.timeout).content, "html.parser")
@@ -50,7 +50,7 @@ class BaiduIncludedCheckUtil(PluginBase):
                 except Exception as e:
                     pass
                 else:
-                    #待查URL匹配百度搜索结果的真实URL，如果匹配就表示收录，循环完毕仍未匹配则未收录, 返回False
+                    # 待查URL匹配百度搜索结果的真实URL，如果匹配就表示收录，循环完毕仍未匹配则未收录, 返回False
                     curl = url.split("://")[-1] if "://" in url else url
                     if r.url.split("://")[-1] == curl:
                         self._put(url)
